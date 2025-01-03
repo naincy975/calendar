1d3eaf6709c99239c8962dba4ab20f0aaebdd978
Naincy Rastogi <naincyrastogi975@gmail.com>
2025-01-03 15:57:19 +0530

    Created the endpoint for getting the month panjika data.

--- main.py
+++ main.py
@@ -5,25 +5,26 @@ from fastapi import FastAPI, Query
 import JivaCalendar_FrontEnd as jcf
 import hvv as hvv
 import General_Vrata as gv
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # React app URL during development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/vrata")
-async def month_vrata():
-    year_s, month_s, year_e, month_e, latitude, longitude = (
-        2024,
-        1,
-        2025,
-        3,
-        27.34,
-        77.40,
-    )
-    data = jcf.get_year_data(year_s, month_s, year_e, month_e, latitude, longitude)
-    vratas = gv.general_vrata(data)
-    return vratas
+async def month_vrata(year: int, month: int, latitude: float, longitude: float):
+    data = jcf.get_month_data(year, month, latitude, longitude)
+    return data
 
+@app.get("/year")
+async def year_vrata(year: int, latitude: float, longitude: float):
+    data = jcf.get_year_data(year, latitude, longitude)
+    return data
 
 @app.get("/special_vrata")
 async def special_vrata():
@@ -36,30 +37,28 @@ async def special_vrata():
         77.40,
     )
     data = jcf.get_year_data(year_s, month_s, year_e, month_e, latitude, longitude)
-    return data
-    # special_vratas = gv.calculate_vrata(data)
-
-    # return special_vratas
+    special_vratas = gv.calculate_vrata(data)
+    return special_vratas
 
 
-if __name__ == "__main__":
-    year_data_file = "year_data.pickle"
-    if not os.path.isfile(year_data_file):
-        with open(year_data_file, "wb") as year_data:
-            year_s, month_s, year_e, month_e, latitude, longitude = (
-                2024,
-                1,
-                2025,
-                3,
-                27.34,
-                77.40,
-            )
-            data = jcf.get_year_data(
-                year_s, month_s, year_e, month_e, latitude, longitude
-            )
-            pickle.dump(data, year_data)
+# if __name__ == "__main__":
+#     year_data_file = "year_data.pickle"
+#     if not os.path.isfile(year_data_file):
+#         with open(year_data_file, "wb") as year_data:
+#             year_s, month_s, year_e, month_e, latitude, longitude = (
+#                 2024,
+#                 1,
+#                 2025,
+#                 3,
+#                 27.34,
+#                 77.40,
+#             )
+#             data = jcf.get_year_data(
+#                 year_s, month_s, year_e, month_e, latitude, longitude
+#             )
+#             pickle.dump(data, year_data)
 
-    with open(year_data_file, "rb") as year_data:
-        data = pickle.load(year_data)
-    vratas = gv.get_ekadashis(data)
-    print(vratas)
+#     with open(year_data_file, "rb") as year_data:
+#         data = pickle.load(year_data)
+#     vratas = gv.get_ekadashis(data)
+#     print(vratas)
